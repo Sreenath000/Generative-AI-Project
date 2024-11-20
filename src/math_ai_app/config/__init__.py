6b05752678673498950212825db15c44d91f1442
Sreenath000 <bhogathisreenathreddy122@gmail.com>
2024-11-20 19:10:33 +0530

    config updated

---
+++ src/math_ai_app/config/__init__.py
@@ -0,0 +1,11 @@
+from .settings import DATABASE_URL, OPEN_API_KEY, LOGGING_CONFIG,APP_NAME, APP_VERSION
+
+
+__all__ = [
+
+    "DATABASE_URL",
+    "OPENAI_API_KEY",
+    "LOGGING_CONFIG",
+    "APP_NAME",
+    "APP_VERSION"
+]
